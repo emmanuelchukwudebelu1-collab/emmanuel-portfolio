2d9ff194b63566b1ea140bc7c78fc65a26cd9f67
emmanuelchukwudebelu1-collab <emmanuelchukwudebelu1@gmail.com>
2026-09-26 09:00:46 +0100

    Add files via upload

---
+++ Emmanuel_Assignment_1.sql
@@ -0,0 +1,9 @@
+__Create a store_locations table containing a store_id (integer), city (varchar), and manager_name (varchar)
+
+CREATE TABLE store_locations(
+store_id INT,
+city VARCHAR(50),
+manager_name VARCHAR (50)
+);
+
+SELECT * FROM store_locations;
---
+++ Emmanuel_Assignment_2.sql
@@ -0,0 +1,9 @@
+__Create a marketing_campaigns table containing a campaign_id (integer), start_date (date), and budget (decimal)
+
+CREATE TABLE marketing_campaigns(
+campaign_id INT,
+start_date DATE,
+budget DECIMAL (10,2)
+);
+
+SELECT * FROM marketing_campaigns
